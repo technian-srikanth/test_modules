3ba576f4e9f48943fac47fc48bf2d4b992f06f26
technian-srikanth <banothu.srikanth@technians.com>
2026-04-01 12:45:14 +0530

    added

--- remove_file/__manifest__.py
+++ remove_file/__manifest__.py
@@ -1,5 +1,8 @@
 {
     'name': 'remove file',
+    'description': """ 
+    Reduce server storage costs and improve performance by removing physical files that have no database reference.
+    """,
     'author': 'srikanth',
     'data': [
         'data/ir_cron.xml',
